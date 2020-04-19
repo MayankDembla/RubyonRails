937ac17f4a5f314238fff501018df0ab264505b7
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-04-19 14:47:32 +0530

    CRUD Operation are completed and is submiteed before DRY.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources:articles, only: [:show, :index, :new, :create, :edit , :update]
+  
+  
+  # REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
+
+  # resources provided REST-ful routes to rails resources
+  
+  resources:articles #, only: [:show, :index, :new, :create, :edit , :update, :destroy]
+
 end
