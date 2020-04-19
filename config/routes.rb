9d50300d64ac86ffed65990eaf9c4bc3e790f629
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-04-19 16:28:00 +0530

    Add Page Controller as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete) to CRUD actions
 
   # resources provided REST-ful routes to rails resources
