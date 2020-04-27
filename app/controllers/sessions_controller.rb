fabe1ef5f6174dc7c0e3ca3eaf1392030b17a85d
MayankDembla <43243575+MayankDembla@users.noreply.github.com>
2020-04-27 15:28:04 +0530

    Add the User Session and destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,27 @@ def new
 end
 
 def create
-    byebug
+     user = User.find_by(email: params[:session][:email].downcase)
+     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id 
+      flash[:notice] = "Logged in Successfully"
+      redirect_to user
+     else
+      # Always use flash[:notice] when our next step is redirect_to 
+      # flash.now is to render the new template again.
+      # As render is not a new request to the new page which simply display the name over again and not rederecting something new
+      # if in this case we have use flash, the message would have set there even if the new request has been taken place 
+      # say user redirect to some article page, the message wold be shown again then also.
+      # So, flash.now will show only to the next http request and then disappears.
+      flash.now[:alert] = " There something wrong with login details"
+      render 'new'
+     end  
 end
 
 def destroy 
+  session[:user_id] = nil 
+  flash[:notice] = "Logged Out"
+  redirect_to root_path
 end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,14 @@ class UsersController < ApplicationController
 
    def index 
      @users = User.paginate(page: params[:page], per_page: 5 )
+    
    end
    
 
    def show 
       @user  = User.find(params[:id])
       @articles = @user.articles
+      @articles = Article.paginate(page: params[:page], per_page: 5 )
    end 
 
    def new 
